39a407564a9aecf623d755703c780a35c4b40915
SunaCho <80630143+SunaCho@users.noreply.github.com>
2021-04-10 01:03:39 +0900

    Add files via upload

---
+++ 2021-1/1463 1로 만들기.py
@@ -0,0 +1,11 @@
+dp=[0,0,1,1]
+N=int(input())
+
+for i in range(4,N+1):
+    dp.append(dp[i-1]+1)
+    if i%2==0:
+        dp[i]=min(dp[i//2]+1,dp[i])
+    if i%3==0:
+        dp[i]=min(dp[i//3]+1,dp[i])
+
+print(dp[N])
