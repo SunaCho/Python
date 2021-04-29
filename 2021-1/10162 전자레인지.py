33f344576f20f3b5e2ee5c4ece533602c2b67ade
SunaCho <80630143+SunaCho@users.noreply.github.com>
2021-04-30 00:24:59 +0900

    Update and rename 10162 전자레인지(수정중).py to 10162 전자레인지.py

--- 2021-1/10162 전자레인지.py
+++ 2021-1/10162 전자레인지.py
@@ -8,18 +8,18 @@ def timecheck():
     global numA, numB, numC
     if time//300!=0:
         numA+=(time//300)
-        timelist.append(numA)
+    timelist.append(numA)
     if (time-numA*300)//60!=0:
         numB+=((time-numA*300)//60)
-        timelist.append(numB)
+    timelist.append(numB)
     if (time-numA*300-numB*60)//10!=0:
         numC+=((time-numA*300-numB*60)//10)
-        timelist.append(numC)
-    if len(timelist)==0:
+    timelist.append(numC)
+    if (numA*300+numB*60+numC*10)!=time or len(timelist)==0:
         print("-1")
         
 timecheck()
 
-if len(timelist)!=0:
+if (numA*300+numB*60+numC*10)==time and len(timelist)!=0:
     timelist_new=[int (i) for i in timelist]
     print(" ".join(repr(e) for e in timelist_new))
