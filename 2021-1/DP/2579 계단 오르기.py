5e5559bb0d886097296c8b5e4f13c536da74a055
SunaCho <80630143+SunaCho@users.noreply.github.com>
2021-05-21 02:01:57 +0900

    Add files via upload

---
+++ 2021-1/DP/2579 계단 오르기.py
@@ -0,0 +1,14 @@
+N=int(input())
+num=[0]*301
+dp=[0]*301
+for i in range(N):
+    num[i]=int(input())
+
+dp[0]=num[0]
+dp[1]=(num[0]+num[1])
+dp[2]=max(num[0]+num[2],num[1]+num[2])
+
+for i in range(3,N):
+    dp[i]=max(dp[i-2]+num[i],dp[i-3]+num[i-1]+num[i])
+
+print(dp[N-1])
