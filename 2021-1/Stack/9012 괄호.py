4e574e79cf557e778952142d6fdf8b66b1542e1c
SunaCho <80630143+SunaCho@users.noreply.github.com>
2021-06-29 01:50:22 +0900

    Add files via upload

---
+++ 2021-1/Stack/9012 괄호.py
@@ -0,0 +1,18 @@
+Testcase=int(input())
+
+for i in range(Testcase):
+    vps=list(input())
+
+    while len(vps)!=0:
+        if vps[0]==")":
+            print("NO")
+            break
+        else:
+            if ")" in vps:
+                vps.remove(")")
+                vps.remove("(")
+            else:
+                print("NO")
+                break
+    if len(vps)==0:
+        print("YES")
