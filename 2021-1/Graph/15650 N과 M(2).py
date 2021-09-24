0b32318b29943098ad7794ff10661813d3570f3b
SunaCho <80630143+SunaCho@users.noreply.github.com>
2021-09-24 22:23:23 +0900

    Add files via upload

---
+++ 2021-1/Graph/15650 N과 M(2).py
@@ -0,0 +1,14 @@
+N,M=list(map(int,input().split()))
+lst=[]
+
+def dfs(start):
+    if len(lst)==M:
+        print(' '.join(map(str,lst)))
+        return
+    for i in range(start,N+1):
+        if i not in lst:
+            lst.append(i)
+            dfs(i+1)
+            lst.pop()
+
+dfs(1)
