a0a2679b394a0851fa85f510b46d039e50042213
SunaCho <80630143+SunaCho@users.noreply.github.com>
2022-01-21 02:05:06 +0900

    Add files via upload

---
+++ 2021-1/Divide-and-Conquer/2630 색종이 만들기.py
@@ -0,0 +1,27 @@
+import sys
+
+N=int(sys.stdin.readline())
+lst=[list(map(int,sys.stdin.readline().split())) for i in range(N)]
+white=0
+blue=0
+
+def sol(x,y,N):
+    global white
+    global blue
+    color=lst[x][y]
+    for i in range(x,x+N):
+        for j in range(y,y+N):
+            if color!=lst[i][j]:
+                sol(x,y,N//2)
+                sol(x,y+N//2,N//2)
+                sol(x+N//2,y,N//2)
+                sol(x+N//2,y+N//2,N//2)
+                return
+    if color==0:
+        white+=1
+    else:
+        blue+=1
+
+sol(0,0,N)
+print(white)
+print(blue)
